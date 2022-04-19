125f349948f5f6e36e17c8f8f040ff50e184ec8c
rimunanda-15 <arimrimunanda15@gmail.com>
2022-04-19 15:17:21 +0700

    revisi renaldy

--- Faktorial.java
+++ Faktorial.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Faktorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Proses faktorial = new Proses();
+        System.out.println("masukan angka : ");
         int bilangan = input.nextInt();
         System.out.print(bilangan + "! = ");
-        int hasil = faktorial(bilangan);
+        int hasil = faktorial.faktorial(bilangan);
         System.out.println();
         System.out.println("Hasil faktorialnya adalah " + hasil);
 
     }
-
-    public static int faktorial(int angka) {
+}
+ class Proses{
+    static int faktorial(int angka) {
         
         if (angka <= 1) {
             System.out.println(" "+angka);
